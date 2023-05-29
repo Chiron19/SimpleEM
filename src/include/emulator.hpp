2e1a5c544ad8695044d862dcb87a73ff589772fb
Konrad Litwinski <klitwinski41418@gmail.com>
2023-05-29 21:18:49 +0200

    Minor code refactoring in Emulator.hpp

--- src/include/emulator.hpp
+++ src/include/emulator.hpp
@@ -166,45 +166,6 @@ void Emulator::kill_emulation() {
 	Logger::print_string_safe("EMULATION KILLED\n");
 }
 
-em_id_t Emulator::choose_next_proc() const {
-    em_id_t earliest_emproc = 0;
-    for (em_id_t em_id = 1; em_id < procs; ++em_id) {
-        if (emprocs[em_id].proc_runtime < emprocs[earliest_emproc].proc_runtime)
-            earliest_emproc = em_id;
-    }
-    return earliest_emproc;
-}
-
-struct timespec Emulator::get_time_interval(em_id_t em_id) const {
-    struct timespec result_ts = 2 * network.get_max_latency();
-
-    for (em_id_t other_proc = 0; other_proc < procs; ++other_proc) {
-        if (other_proc == em_id)
-            continue;
-        result_ts = std::min(result_ts, emprocs[other_proc].proc_runtime - 
-            emprocs[em_id].proc_runtime + network.get_latency(other_proc, em_id));
-    }
-    return result_ts;
-}
-
-void Emulator::schedule_sent_packets(em_id_t em_id) {
-    while(!emprocs[em_id].out_packets.empty()) {
-        Packet packet = emprocs[em_id].out_packets.top();
-        emprocs[em_id].out_packets.pop();
-
-        if (packet.get_version() != 4)
-            continue; // FIXME temporary fix of random packets
-
-        em_id_t dest_em_id = network.get_em_id(packet.get_dest_addr());
-        packet.increase_ts(network.get_latency(em_id, dest_em_id));
-
-        packet.set_dest_addr(network.get_inter_addr());
-        packet.set_source_addr(network.get_addr(em_id));
-
-        emprocs[dest_em_id].in_packets.push(packet);
-    }
-}
-
 void Emulator::fork_stop_run(int* pids, const ConfigParser& cp) {
     int status;
 
@@ -250,4 +211,43 @@ void Emulator::child_init(
 
 	printf("[TINYEM] Process %d finished all the work\n", getpid());
 	return;
+}
+
+em_id_t Emulator::choose_next_proc() const {
+    em_id_t earliest_emproc = 0;
+    for (em_id_t em_id = 1; em_id < procs; ++em_id) {
+        if (emprocs[em_id].proc_runtime < emprocs[earliest_emproc].proc_runtime)
+            earliest_emproc = em_id;
+    }
+    return earliest_emproc;
+}
+
+struct timespec Emulator::get_time_interval(em_id_t em_id) const {
+    struct timespec result_ts = 2 * network.get_max_latency();
+
+    for (em_id_t other_proc = 0; other_proc < procs; ++other_proc) {
+        if (other_proc == em_id)
+            continue;
+        result_ts = std::min(result_ts, emprocs[other_proc].proc_runtime - 
+            emprocs[em_id].proc_runtime + network.get_latency(other_proc, em_id));
+    }
+    return result_ts;
+}
+
+void Emulator::schedule_sent_packets(em_id_t em_id) {
+    while(!emprocs[em_id].out_packets.empty()) {
+        Packet packet = emprocs[em_id].out_packets.top();
+        emprocs[em_id].out_packets.pop();
+
+        if (packet.get_version() != 4)
+            continue; // FIXME temporary fix of random packets
+
+        em_id_t dest_em_id = network.get_em_id(packet.get_dest_addr());
+        packet.increase_ts(network.get_latency(em_id, dest_em_id));
+
+        packet.set_dest_addr(network.get_inter_addr());
+        packet.set_source_addr(network.get_addr(em_id));
+
+        emprocs[dest_em_id].in_packets.push(packet);
+    }
 }
